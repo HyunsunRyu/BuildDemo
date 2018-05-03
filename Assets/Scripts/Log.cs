cacea3a12dcbe2ce7ba20a97979fddecf0105cde
HyunsunRyu <lovedangyou@gmail.com>
2018-05-04 01:07:03 +0900

    add

---
+++ Assets/Scripts/Log.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Log : MonoBehaviour
+{
+    public Text text;
+
+    private static Log instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public static void Set(string data)
+    {
+        if (instance != null)
+            instance.text.text = data;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
