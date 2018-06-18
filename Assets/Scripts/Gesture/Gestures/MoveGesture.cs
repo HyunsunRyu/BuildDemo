6d0890a793e542fa2563b6f1663f60c2f565b7a9
HyunsunRyu <lovedangyou@gmail.com>
2018-06-18 23:22:14 +0900

    add building objects and remove earth objects

---
+++ Assets/Scripts/Gesture/Gestures/MoveGesture.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MoveGesture : Gesture
+{
+
+
+    public override void Init()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override bool Recognize()
+    {
+        if (touchCount > 1)
+            return false;
+
+
+
+        throw new System.NotImplementedException();
+    }
+}
